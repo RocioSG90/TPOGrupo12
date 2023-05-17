62a8e53511d56cfebb906630a8403b372e07affb
RocioSG90 <graziano.rocio@gmail.com>
2023-05-17 20:42:38 -0300

    prueba formulario

---
+++ JS/formulario.js
@@ -0,0 +1,4 @@
+//function captura () {
+      //  var nomcompleto=document.getElementById("nombrecompleto").value;
+     //   var direcciondecorreo=document.getElementById("direcciondecorreo").value;
+     //   console.log(nombrecompleto + " " direcciondecorreo);}
